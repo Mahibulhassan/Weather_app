1772e05a0f257d499521d121089fcdd59585f9ee
Mahibulhassan <mahibulhassan842@gmail.com>
2021-04-30 01:28:06 +0600

    base structure added

---
+++ Weather_app/app/src/main/java/com/mahibul/weather_app/Ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mahibul.weather_app.Ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mahibul.weather_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
